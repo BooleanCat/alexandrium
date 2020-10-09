3d340f221a8252377f962c6c8bd507043d282cfa
Tom Godkin <tomgodkin@pm.me>
2020-10-09 21:10:21 +0100

    Validate identifers for GET requests

--- router/router.go
+++ router/router.go
@@ -2,32 +2,31 @@ package router
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"regexp"
 
 	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 
 	"github.com/BooleanCat/alexandrium/store"
+	"github.com/BooleanCat/alexandrium/types"
 )
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 //counterfeiter:generate -o ./internal/fake_books.go github.com/BooleanCat/alexandrium/store.Books
 //counterfeiter:generate -o ./internal/fake_authors.go github.com/BooleanCat/alexandrium/store.Authors
 
+var isbn13Pattern = regexp.MustCompile(`\d{13}`)
+
 // New creates alexandrium's HTTP router.
 func New(books store.Books, authors store.Authors) *chi.Mux {
 	router := chi.NewRouter()
 
-	uuid4Pattern := "[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}"
-
 	router.Get("/ping", HandlePing)
 
-	router.Route("/books", func(r chi.Router) {
-		r.Get(fmt.Sprintf("/{id:%s}", uuid4Pattern), HandleGetBook(books))
-		r.Get("/{isbn}", HandleGetBookByISBN(books))
-	})
+	router.Get("/books/{id}", HandleGetBook(books))
 
-	router.Get(fmt.Sprintf("/authors/{id:%s}", uuid4Pattern), HandleGetAuthor(authors))
+	router.Get("/authors/{id}", HandleGetAuthor(authors))
 
 	return router
 }
@@ -37,36 +36,20 @@ func HandlePing(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
-// HandleGetBook responds with a book by ISBN
+// HandleGetBook responds with a book by ISBN or ID
 func HandleGetBook(books store.Books) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
 
 		w.Header().Set("Content-Type", "application/json")
 
-		book, err := books.ByID(id)
-		if store.IsNotFound(err) {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		} else if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+		finder := getBookStrategy(books, id)
+		if finder == nil {
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		if err := json.NewEncoder(w).Encode(&book); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	}
-}
-
-// HandleGetBookByISBN responds with a book by ISBN
-func HandleGetBookByISBN(books store.Books) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		isbn := chi.URLParam(r, "isbn")
-
-		w.Header().Set("Content-Type", "application/json")
-
-		book, err := books.ByISBN(isbn)
+		book, err := finder(id)
 		if store.IsNotFound(err) {
 			w.WriteHeader(http.StatusNotFound)
 			return
@@ -87,6 +70,11 @@ func HandleGetAuthor(authors store.Authors) func(http.ResponseWriter, *http.Requ
 
 		w.Header().Set("Content-Type", "application/json")
 
+		if _, err := uuid.Parse(id); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		author, err := authors.ByID(id)
 		if store.IsNotFound(err) {
 			w.WriteHeader(http.StatusNotFound)
@@ -100,4 +88,16 @@ func HandleGetAuthor(authors store.Authors) func(http.ResponseWriter, *http.Requ
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	}
+}
+
+func getBookStrategy(books store.Books, id string) func(string) (types.Book, error) {
+	if _, err := uuid.Parse(id); err == nil {
+		return books.ByID
+	}
+
+	if isbn13Pattern.MatchString(id) {
+		return books.ByISBN
+	}
+
+	return nil
 }
--- router/router_test.go
+++ router/router_test.go
@@ -40,91 +40,19 @@ var _ = Describe("Router", func() {
 		})
 	})
 
-	Describe("/books/{isbn}", func() {
-		var response *http.Response
-
-		BeforeEach(func() {
-			fakeBooks.ByISBNReturns(types.Book{ISBN: "9781788547383"}, nil)
-		})
-
-		JustBeforeEach(func() {
-			response = httpGet(server.URL + "/books/9781788547383")
-		})
-
-		AfterEach(func() {
-			Expect(response.Body.Close()).To(Succeed())
-		})
-
-		It("responds", func() {
-			By("having status code 200 OK", func() {
-				Expect(response.StatusCode).To(Equal(http.StatusOK))
-			})
-
-			By("having the correct Content-Type", func() {
-				Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
-			})
-
-			By("having searched by ISBN", func() {
-				Expect(fakeBooks.ByISBNCallCount()).To(Equal(1))
-			})
-
-			By("delivering the book in the response body", func() {
-				var book types.Book
-				Expect(json.NewDecoder(response.Body).Decode(&book)).To(Succeed())
-				Expect(book.ISBN).To(Equal("9781788547383"))
-			})
-		})
-
-		When("the book isn't found", func() {
-			BeforeEach(func() {
-				fakeBooks.ByISBNReturns(types.Book{}, store.NotFoundError{})
-			})
-
-			It("responds", func() {
-				By("having status code 404 Not Found", func() {
-					Expect(response.StatusCode).To(Equal(http.StatusNotFound))
-				})
-
-				By("having the correct Content-Type", func() {
-					Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
-				})
-
-				By("having searched by ISBN", func() {
-					Expect(fakeBooks.ByISBNCallCount()).To(Equal(1))
-				})
-			})
-		})
-
-		When("searching by ISBN fails", func() {
-			BeforeEach(func() {
-				fakeBooks.ByISBNReturns(types.Book{}, errors.New("oops"))
-			})
-
-			It("responds", func() {
-				By("having status code 500 Internal Server Error", func() {
-					Expect(response.StatusCode).To(Equal(http.StatusInternalServerError))
-				})
-
-				By("having the correct Content-Type", func() {
-					Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
-				})
-
-				By("having searched by ISBN", func() {
-					Expect(fakeBooks.ByISBNCallCount()).To(Equal(1))
-				})
-			})
-		})
-	})
-
 	Describe("/books/{id}", func() {
-		var response *http.Response
+		var (
+			response *http.Response
+			id       string
+		)
 
 		BeforeEach(func() {
-			fakeBooks.ByIDReturns(types.Book{ID: "76341e07-911c-44fd-aafa-13b43daf3494"}, nil)
+			id = "76341e07-911c-44fd-aafa-13b43daf3494"
+			fakeBooks.ByIDReturns(types.Book{ID: id}, nil)
 		})
 
 		JustBeforeEach(func() {
-			response = httpGet(server.URL + "/books/76341e07-911c-44fd-aafa-13b43daf3494")
+			response = httpGet(server.URL + "/books/" + id)
 		})
 
 		AfterEach(func() {
@@ -147,7 +75,7 @@ var _ = Describe("Router", func() {
 			By("delivering the book in the response body", func() {
 				var book types.Book
 				Expect(json.NewDecoder(response.Body).Decode(&book)).To(Succeed())
-				Expect(book.ID).To(Equal("76341e07-911c-44fd-aafa-13b43daf3494"))
+				Expect(book.ID).To(Equal(id))
 			})
 		})
 
@@ -171,6 +99,26 @@ var _ = Describe("Router", func() {
 			})
 		})
 
+		When("searching by an invalid ID", func() {
+			BeforeEach(func() {
+				id = "not-valid"
+			})
+
+			It("responds", func() {
+				By("having status code 400 Bad Request", func() {
+					Expect(response.StatusCode).To(Equal(http.StatusBadRequest))
+				})
+
+				By("having the correct Content-Type", func() {
+					Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
+				})
+
+				By("not having searched by ID", func() {
+					Expect(fakeBooks.ByIDCallCount()).To(BeZero())
+				})
+			})
+		})
+
 		When("searching by ID fails", func() {
 			BeforeEach(func() {
 				fakeBooks.ByIDReturns(types.Book{}, errors.New("oops"))
@@ -190,17 +138,88 @@ var _ = Describe("Router", func() {
 				})
 			})
 		})
+
+		When("searching by ISBN", func() {
+			BeforeEach(func() {
+				id = "9781788547383"
+				fakeBooks.ByISBNReturns(types.Book{ISBN: "9781788547383"}, nil)
+			})
+
+			It("responds", func() {
+				By("having status code 200 OK", func() {
+					Expect(response.StatusCode).To(Equal(http.StatusOK))
+				})
+
+				By("having the correct Content-Type", func() {
+					Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
+				})
+
+				By("having searched by ISBN", func() {
+					Expect(fakeBooks.ByISBNCallCount()).To(Equal(1))
+				})
+
+				By("delivering the book in the response body", func() {
+					var book types.Book
+					Expect(json.NewDecoder(response.Body).Decode(&book)).To(Succeed())
+					Expect(book.ISBN).To(Equal("9781788547383"))
+				})
+			})
+
+			When("the book isn't found", func() {
+				BeforeEach(func() {
+					fakeBooks.ByISBNReturns(types.Book{}, store.NotFoundError{})
+				})
+
+				It("responds", func() {
+					By("having status code 404 Not Found", func() {
+						Expect(response.StatusCode).To(Equal(http.StatusNotFound))
+					})
+
+					By("having the correct Content-Type", func() {
+						Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
+					})
+
+					By("having searched by ISBN", func() {
+						Expect(fakeBooks.ByISBNCallCount()).To(Equal(1))
+					})
+				})
+			})
+
+			When("searching by ISBN fails", func() {
+				BeforeEach(func() {
+					fakeBooks.ByISBNReturns(types.Book{}, errors.New("oops"))
+				})
+
+				It("responds", func() {
+					By("having status code 500 Internal Server Error", func() {
+						Expect(response.StatusCode).To(Equal(http.StatusInternalServerError))
+					})
+
+					By("having the correct Content-Type", func() {
+						Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
+					})
+
+					By("having searched by ISBN", func() {
+						Expect(fakeBooks.ByISBNCallCount()).To(Equal(1))
+					})
+				})
+			})
+		})
 	})
 
 	Describe("/authors/{id}", func() {
-		var response *http.Response
+		var (
+			response *http.Response
+			id       string
+		)
 
 		BeforeEach(func() {
-			fakeAuthors.ByIDReturns(types.Author{ID: "ea1ff7d7-67cd-477c-8cb7-8756619e275d"}, nil)
+			id = "ea1ff7d7-67cd-477c-8cb7-8756619e275d"
+			fakeAuthors.ByIDReturns(types.Author{ID: id}, nil)
 		})
 
 		JustBeforeEach(func() {
-			response = httpGet(server.URL + "/authors/ea1ff7d7-67cd-477c-8cb7-8756619e275d")
+			response = httpGet(server.URL + "/authors/" + id)
 		})
 
 		AfterEach(func() {
@@ -223,7 +242,7 @@ var _ = Describe("Router", func() {
 			By("delivering the author in the response body", func() {
 				var author types.Author
 				Expect(json.NewDecoder(response.Body).Decode(&author)).To(Succeed())
-				Expect(author.ID).To(Equal("ea1ff7d7-67cd-477c-8cb7-8756619e275d"))
+				Expect(author.ID).To(Equal(id))
 			})
 		})
 
@@ -263,6 +282,26 @@ var _ = Describe("Router", func() {
 
 				By("having searched by ID", func() {
 					Expect(fakeAuthors.ByIDCallCount()).To(Equal(1))
+				})
+			})
+		})
+
+		When("searching by an invalid ID", func() {
+			BeforeEach(func() {
+				id = "not-valid"
+			})
+
+			It("responds", func() {
+				By("having status code 400 Bad Request", func() {
+					Expect(response.StatusCode).To(Equal(http.StatusBadRequest))
+				})
+
+				By("having the correct Content-Type", func() {
+					Expect(response.Header.Get("Content-Type")).To(Equal("application/json"))
+				})
+
+				By("having not having searched by ID", func() {
+					Expect(fakeAuthors.ByIDCallCount()).To(BeZero())
 				})
 			})
 		})
